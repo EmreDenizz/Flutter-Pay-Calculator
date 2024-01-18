64c7858c0211a3512ac878eb1a8debf5c44f4276
Emre Deniz <emre.deniz@outlook.com>
2024-01-18 01:52:41 -0500

    Define input widgets

---
+++ android/app/src/main/kotlin/com/example/emredeniz_assign1_mapd722/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.emredeniz_assign1_mapd722
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const appTitle = 'Pay Calculator';
+
+    return MaterialApp(
+        title: appTitle,
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text(appTitle),
+            ),
+            body: const MyForm(),
+            resizeToAvoidBottomInset: false));
+  }
+}
+
+class MyForm extends StatefulWidget {
+  const MyForm({super.key});
+
+  @override
+  MyFormState createState() {
+    return MyFormState();
+  }
+}
+
+class MyFormState extends State<MyForm> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController numberOfHours = TextEditingController();
+  TextEditingController hourlyRate = TextEditingController();
+
+  // Define output result texts
+  String regularPayOutput = "regular pay: ";
+  String overtimePayOutput = "overtime pay: ";
+  String totalPayOutput = "total pay: ";
+  String taxOutput = "tax: ";
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a form widget using the _formKey
+    return Container(
+        color: const Color.fromRGBO(38, 38, 38, 0.4),
+        child: Column(children: [
+          Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Text input: "Number of hours"
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                  child: TextFormField(
+                    controller: numberOfHours,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      filled: true,
+                      fillColor: Colors.white,
+                      hintText: 'Number of hours',
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a value';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                // Text input: "Hourly rate"
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                  child: TextFormField(
+                    controller: hourlyRate,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      filled: true,
+                      fillColor: Colors.white,
+                      hintText: 'Hourly rate',
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a value';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                // Button: "Calculate"
+              ],
+            ),
+          ),
+          // Output report
+
+          // About part
+        ]));
+  }
+}
